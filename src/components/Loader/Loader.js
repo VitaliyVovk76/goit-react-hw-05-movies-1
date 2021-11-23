47c25c12197306253192cb286278b584f0a0b45b
VitaliyVovk76 <vovkvitol@gmail.com>
2021-11-23 18:39:37 +0200

    удалил лишние файлы

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,7 +1,9 @@
 import Load from "react-loader-spinner";
+import s from "../Loader/Loader.module.css";
 export default function Loader() {
   return (
     <Load
+      classname={s.loader}
       type="Circles"
       color="#00BFFF"
       height={50}
